010c2bf13809abe8e7bcfe8a0360b9aa65bb03a0
Andry85 <andrii.shapoval@smile-ukraine.com>
2021-02-03 17:23:37 +0200

    added nav

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import styles from './Header.module.scss';
 
+import Nav from './Nav';
+
 function Header() {
   return (
     <header className={styles.header}>
-      
+      <Nav/>
     </header>
   );
 }
---
+++ client/src/components/Header/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styles from './Nav.module.scss';
+
+function Nav() {
+  return (
+    <nav className={styles.nav}>
+      hii
+    </nav>
+  );
+}
+
+export default Nav;
